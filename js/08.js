bae9ddc380571b1844fd3b504f830cca39d1b458
RIKURI <Riku@net84-dhcp-22.sfc.keio.ac.jp>
2015-11-26 11:19:45 +0900

    テスト２

--- js/08.js
+++ js/08.js
@@ -2,9 +2,13 @@
 function isLeapYear(a){
 
   var A = (a % 100 == 0);
+  var B = (a % 400 == 0);
 
   if(a % 4 == 0 && !A){
     return true;
   }else{
+    if(a % 4 == 0 && A && !B){
+      return false;
+    }
   }
 }
